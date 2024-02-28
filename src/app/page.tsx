5881aa30e2c6adde0cd35d26f5493f595e8954c4
Camille Adam <camilleadam006@gmail.com>
2024-02-28 18:06:10 +0100

    create components for todays's weather

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Navbar from "@/components/Navbar";
-import Image from "next/image";
-
-export default function Home() {
-  return (<div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-    <Navbar/>
-  </div>)
-}
